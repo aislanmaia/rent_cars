102ef74dbd2580a09271eda7861aa114b9752759
Aislan Maia <aislan.maia@totvs.com.br>
2024-02-27 01:24:17 -0300

    feat: create a category resource and schema

--- test/rent_cars_web/controllers/api/category_controller_test.exs
+++ test/rent_cars_web/controllers/api/category_controller_test.exs
@@ -4,12 +4,6 @@ defmodule RentCarsWeb.Api.CategoryControllerTest do
   test "list all categories", %{conn: conn} do
     conn = get(conn, ~p"/api/categories")
 
-    assert json_response(conn, 200)["data"] == [
-             %{
-               "description" => "pumpkin 123",
-               "id" => "123",
-               "name" => "SPORT"
-             }
-           ]
+    assert json_response(conn, 200)["data"] == []
   end
 end
