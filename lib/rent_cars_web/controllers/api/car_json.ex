491708cc1022227d64f10ad0fecadf879c8b02fb
Aislan Maia <aislan.maia@totvs.com.br>
2024-03-22 15:48:08 -0300

    fix: credo issue

--- lib/rent_cars_web/controllers/api/car_json.ex
+++ lib/rent_cars_web/controllers/api/car_json.ex
@@ -1,7 +1,9 @@
 defmodule RentCarsWeb.Api.CarJSON do
+  alias RentCarsWeb.Api.Admin.CarJSON
+
   def index(%{cars: cars}) do
-    %{data: for(car <- cars, do: RentCarsWeb.Api.Admin.CarJSON.show(%{car: car}))}
+    %{data: for(car <- cars, do: CarJSON.show(%{car: car}))}
   end
 
-  def show(%{car: car}), do: %{data: RentCarsWeb.Api.Admin.CarJSON.show(%{car: car})}
+  def show(%{car: car}), do: %{data: CarJSON.show(%{car: car})}
 end
