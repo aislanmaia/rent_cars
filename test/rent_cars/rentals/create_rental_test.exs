df7a7b211be09a2f46d8943a38a34414e5d2d33a
Aislan Maia <aislan.maia@totvs.com.br>
2024-03-23 11:40:40 -0300

    feat(wip): create rental - add check car availability

---
+++ test/rent_cars/rentals/create_rental_test.exs
@@ -0,0 +1,13 @@
+defmodule RentCars.Rentals.CreateRentalTest do
+  use RentCars.DataCase
+  alias RentCars.Rentals.CreateRental
+  import RentCars.CarsFixtures
+
+  describe "create rentals" do
+    test "throw error when car is not available" do
+      car = car_fixture(%{available: false})
+
+      assert {:error, "Car is unavailable"} == CreateRental.execute(car.id)
+    end
+  end
+end
